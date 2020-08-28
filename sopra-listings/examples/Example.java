4d8bf9af42f6f34686cf67397ab87772df7d213c
EagleoutIce <maeusepiep99@web.de>
2020-08-28 15:20:20 +0200

    Updated the actions and added an example to shocase listings with color-palettes support
    Signed-off-by: EagleoutIce <maeusepiep99@web.de>

---
+++ sopra-listings/examples/Example.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+// Further imports, maybe
+
+|ihl|Diese Zeile wird künstlich hervorgehoben|ihl|
+
+@ImportantAnnotation
+public class Example {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) { /* go on forever */
+            String s = scanner.nextLine();
+            if (s.equals("exit"))
+                break;
+            System.out.println(s);
+        }
+        System.out.println("2 + 2 is " + (2+2));
+        scanner.close();
+    }
+}
