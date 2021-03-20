ab37467f0d733f0f7cfb60d0bded8c84ea75c866
EagleoutIce <maeusepiep99@web.de>
2021-03-20 13:23:34 +0100

    polish the installer .py

--- installer.py
+++ installer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 # Installer for 'sopra-*' LaTeX Packages
-
 import os
 import sys
 from pathlib import Path
@@ -13,7 +12,6 @@ print("     - texlive on linux: '<home>/texmf/'")
 print("     - macTeX on Darwin: '/Users/<yourUsername>/Library/texmf/'")
 
 target_path = os.path.expanduser(input('TEXMF> '))
-
 print("Using: '%s'" % (target_path))
 
 if not os.path.isdir(target_path):
@@ -29,45 +27,45 @@ if not os.path.isdir(target_path):
 
 print("Generating texmf-tree...")
 target_fm = Path(target_path) / 'tex' / 'latex' / 'sopra-packages'
-(target_fm).mkdir(parents=True,exist_ok=True)
+(target_fm).mkdir(parents=True, exist_ok=True)
 
 
-def getPkgClsName(dirname : str):
+def get_pkg_cls_name(dirname: str) -> str:
     # Check if dirname is a class or package
     if os.path.isfile(Path.cwd() / Path(dirname) / (dirname + '.sty')):
         return dirname + '.sty'
     else:
         return dirname + '.cls'
 
-def updateOrInstallComponent(source_file : str, tar_dirname : str, tar_filename:str,indent:int):
+
+def update_or_install_component(source_file: Path, tar_dirname: Path, tar_filename: str, indent: int) -> None:
     # Check for dir creation:
-    (tar_dirname/tar_filename).parent.mkdir(parents=True,exist_ok=True)
+    (tar_dirname/tar_filename).parent.mkdir(parents=True, exist_ok=True)
     # print("called with: " + tar_filename)
     if not os.path.isfile(tar_dirname/tar_filename):
         print(" "*indent + "Installing: '%s'" % tar_filename)
-        copyfile(source_file,tar_dirname / tar_filename)
+        copyfile(source_file, tar_dirname / tar_filename)
     elif os.path.getmtime(source_file) > os.path.getmtime(tar_dirname/tar_filename):
         print(" "*indent + "Updating: '%s'" % tar_filename)
-        copyfile(source_file,tar_dirname / tar_filename)
+        copyfile(source_file, tar_dirname / tar_filename)
     else:
         print(" "*indent + "Is up to date: '%s'" % tar_filename)
+
 
 # Walk current path and filter for sopra'
 for root, dirs, files in os.walk("."):
     for dirname in dirs:
         if "sopra-" in dirname:
-            
-            filename = getPkgClsName(dirname)
+            filename = get_pkg_cls_name(dirname)
             source_dir = Path.cwd() / Path(dirname)
             source_file = source_dir / filename
-            updateOrInstallComponent(source_file, target_fm, filename,0)
-
+            update_or_install_component(source_file, target_fm, filename, 0)
             # We'll now check, if there are additional installer-files
             extra_f = source_dir/".install-extras"
             if os.path.isfile(extra_f):
                 print("  * Package has extras!")
                 with open(extra_f) as read_extras:
                     for f in read_extras.readlines():
-                        f = f.replace("\n","")
-                        updateOrInstallComponent(source_dir/f,target_fm,f,4)
-                    
+                        f = f.replace("\n", "")
+                        update_or_install_component(
+                            source_dir/f, target_fm, f, 4)
